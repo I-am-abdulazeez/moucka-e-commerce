2dc9b18f5c9d84e4d6c83fcb547ce3af3956a0f9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2020-12-04 00:51:57 +0100

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 const cors = require("cors");
 require("colors");
 const connectDB = require("./config/db");
@@ -17,13 +18,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/orderings", orderings);
 
-if (["production"].includes(process.env.NODE_ENV)) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 app.listen(PORT, () =>
