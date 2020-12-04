151eddfbab413a16313d62ae4be23d6ad4180270
AbdulAzeez <abdulazeezolamide50@gmail.com>
2020-12-04 10:36:18 +0100

    first commit

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/orderings", orderings);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 app.listen(PORT, () =>
   console.log(
