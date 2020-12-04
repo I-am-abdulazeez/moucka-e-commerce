f4e9f6a6c4d085c0c736d5e9a7af89436cf77883
AbdulAzeez <abdulazeezolamide50@gmail.com>
2020-12-04 10:42:02 +0100

    first commit o

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const orderings = require("./routes/orders");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/api/v1/orderings", orderings);
 
